7abd3b21931f6b5a29a801a831870ce2812b9415
Aubrey Lamb <aubreyrlamb@gmail.com>
2023-04-15 13:44:23 -0600

    adding questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,13 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What's the name of your project?", "What is the name of your Github repo?", "What is your Github username?"];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
